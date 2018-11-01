1d2f0e93fa8b673a65fb6bd619a250c968d78991
LyuHaiLoong <lyuhailoong@163.com>
2018-11-01 19:00:53 +0800

    20181101 version-half

---
+++ CSS-JS demos/TouTiao-homepage/JS/index.js
@@ -0,0 +1,18 @@
+(function() {
+	const content = document.querySelectorAll(".standard-content");
+	// Array.from(a);
+	const contentAll = Array.prototype.slice.call(content);
+
+
+	for (let val of contentAll) {
+		val.onclick = function() {
+			this.classList.toggle("in");
+		}
+	}
+
+	const nav = document.querySelector(".nav-top");
+
+	window.onscroll = () => {
+		nav.style.top = 0;
+	}
+})()
